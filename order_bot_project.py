f34b42b0a06d9888581c91d05332be808bd47d62
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2022-06-24 15:31:05 -0700

    removed old language

--- order_bot_project.py
+++ order_bot_project.py
@@ -3,11 +3,11 @@
 	# You've just learned about variables, conditionals, functions, and user input. 
 	# You've also created a basic calculator in a previous project.
 	
-	# Now imagine you are going out to eat with two other friends.
+	# Now imagine you are going out to eat.
 	# Are you at a restaurant for a meal? Are you grabbing boba? Or are you just going to an ice cream parlor?
-	# At the end of the meal, you and your friends have to split the bill. 
+	# At the end of the meal, you get the bill. 
 
-	# Wouldn't it be great if we could automate that math?
+	# How do you think restaurants automate that math?
 
 					# Let's try it!
 
@@ -15,13 +15,19 @@
 
 # Scenario Parameters: 
 
-	# When you and your friends eat out, each of you have the option to order one or multiple items.
-	# What kind of items are available to order?
+	# When you eat out, you have the option to order one or multiple items.
+	# What kind of items are available to order? There's usually a menu.
+	# Allow your user to order a drink, meal, and dessert.
 
-	# At the end of the order, the receipt comes and you all have to calculate the cost for each person:
+	# At the end of the order, the receipt comes and you have to calculate the total cost:
 	# Don't forget the tax and tip!
 
-# After this program finishes running, it should output a specific total for each person
+# After this program finishes running, it should output a receipt with:
+        #1. the items you ordered and their cost 
+	#2. a total for your order before tax
+	#3. a total for your order after tax
+	#4. the amount of your tip 
+	#5. the total including tax and tip
 
 # -------------------------------------------- 
 
@@ -29,13 +35,40 @@
 # -------------------------------------------- 
 
 # Part 1:
-# Let's start by asking taking the order of the group(you and two friends). What did each person order?
+# Let's start by creating the variables we'll need to keep track of the user's order
+# as well as TAX and tip
 
-# Write a function that will take the group's order:
-# - Prompt the user to enter the price of each item they ordered
+# Remember: Your user should be able to order at least 3 items (a drink, meal, and dessert item). 
+
+# --------------------------------------------
+
+
+
+# -------------------------------------------- 
+
+# Part 2:
+# Next, let's display the menu. Include the food item name and it's cost. 
+
+# Write a function that will display the menu:
+# - Print each item available and it's cost. You should have at least 3 items available (a drink, meal, and dessert item). 
+
+# --------------------------------------------
+
+
+
+
+
+
+# -------------------------------------------- 
+
+# Part 3:
+# Let's take the order. What did the user order? What does it cost?
+
+# Write a function that will take the order:
+# - Prompt the user to enter a drink, dessert, and meal (Bonus: give them the option to not order an item)
 # - Return the cost 
 
-# Remember! Functions are meant to be reusable, so write a function that will work when called for each person!
+# Remember! Functions are meant to be reusable, so write a function that will work when called repeatedly!
 
 # --------------------------------------------
 
@@ -52,11 +85,11 @@
 
 # -------------------------------------------- 
 
-# Part 2:
-# Now that you have the costs of everything ordered, let's calculate the cost of each person's order(including tip and tax).
+# Part 4:
+# Now that you have the costs of everything ordered, let's calculate the cost of the order(including tip and tax).
 
-# Write a function that will calculate the cost of each person's order, including:
-# - Cost of all of their ordered items
+# Write a function that will calculate the cost of the order, including:
+# - Cost of all  ordered items
 # - Tax (Look up the sales tax of your city)
 # - Tip (Give the user the option to enter how much they want to tip)
 
@@ -77,38 +110,45 @@
 
 # -------------------------------------------- 
 
-# Part 3:
-# Let's print out a receipt for each person.
+# Part 5:
+# Let's print out a receipt.
 
-# Write a function that will take the values of each person's order and total cost and print it out in an appealing way.
+# Write a function that will take the values of the order and total cost and print it out in an appealing way.
 
 # The receipt should include:
 # - Cost of each item
-# - Total cost for each person
+# - Tax for the order
+# - Tip for the order
+# - Total cost for the order
 
-# Remember! Functions are meant to be reusable, so write a function that will work when called for each person!
 
 # -------------------------------------------- 
-
-
-
-
-
-
 
 
 
 
 # -------------------------------------------- 
 
-# Part 4: Upchallenges!
+# Part 6: Food Order Bot
+
+# Call all of your functions to get your food order bot up and running!
+
+# --------------------------------------------
+
+
+
+
+
+
+
+# -------------------------------------------- 
+
+# Part 7: Upchallenges!
 
 # How many of these upchallenges can you implement?
 
-# - Make sure the user is only entering numbers. If they enter an invalid value, prompt them to re-enter. 
+# - Make sure the user is only entering valid values. If they enter an invalid value, prompt them to re-enter. 
 # - The displayed prices should only have two decimal places.
-# - Can you adjust your program to account for any shared items ordered for the group?
-# - Display the tax and tip values
 # - Implement a rewards system (stamp cards, buy one get one, etc)
 
 # --------------------------------------------
