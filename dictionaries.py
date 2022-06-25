ef3f6938f535a6f5971758bebdb2a93f7501400e
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2022-06-24 17:36:42 -0700

    added method challenge

--- dictionaries.py
+++ dictionaries.py
@@ -9,7 +9,7 @@
  #  1. DEFINE: Make a dictionary and set its properties, printing changes in between.
  #  2. MODIFY: Add 2 new properties and assing values. Change existing values,
  #     including adding or updating values inside lists
- #  3. METHODS: Now its time to make some methods. Then can simply make a change
+ #  3. METHODS: Now its time to make some methods tha can make a change
  #     to your values, like a boolean, or they can print something about
  #     the dictionary. Nothing fancy, unless you fancy it.
  #  4. LITERALLY: Use string literals to put it all together in one statement.
@@ -60,7 +60,16 @@ print("------------------- CHALLENGE 2 : MODIFY   -------------------")
 #-->TODO: Print your dictionary again and observe changes
 
 
+print("------------------- CHALLENGE 3 : MEHTODS   -------------------")
 
-print("------------------- CHALLENGE 3 : LITERALLY   -------------------")
+
+#-->TODO: Make a method that will update your dictionary value. It should take in a dictionary and return it modified.
+
+
+#-->TODO: Call the method.
+
+
+
+print("------------------- CHALLENGE 4 : LITERALLY   -------------------")
 
 #-->TODO: Put it all together using a string literal to tell the story of your dictionary!
